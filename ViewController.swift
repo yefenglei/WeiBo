2c9e4943e151d747266c8e2be4cbeb0ec1e71a5e
yefenglei <yefenglei@yefengleideMac-mini.local>
2015-09-05 19:34:17 +0800

    An application like Weibo

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeiBo
+//
+//  Created by 叶锋雷 on 15/9/5.
+//  Copyright (c) 2015年 叶锋雷. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
